32f12ab7194aa67086980ff9c29aa827ee62e76d
sato4app <sato.apps@quad.razor.jp>
2025-07-29 22:40:27 +0900

    update all

--- README.md
+++
@@ -1,8 +0,0 @@
-### Tabata Timer（疑似）
-- 準備(Setup)、実施(WorkOut)、休憩(Rest)、セット数(Set Count)の4種の整数値を設定
-- Setupは最初のみ、WorkOutとRestをSet Count数実施。
-- Startで開始、Stopで終了。終了時には開始時の数値に値を戻す。
-
-- 改善を要する点：
-  - WorkOutとRestの値が、残が0になった後で開始時の数値に戻らない。
-  - 終了時は開始時の値に戻したいが、設定の初期値(3, 5, 4, 3)になる。
--- service-worker.js
+++ service-worker.js
@@ -1,56 +1,21 @@
-// Service Workerのバージョン
-const CACHE_NAME = 'tabata-timer-v0.1';
-
-// キャッシュするファイルのリスト
+const CACHE_NAME = 'tabata-timer-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
+  '/manifest.json',
   '/styles.css',
-  '/app.js',
-  '/TimerIcon-192.png',
-  '/TimerIcon-512.png'
 ];
 
-// インストール時の処理
 self.addEventListener('install', event => {
-  console.log('Service Worker Installed');
-  
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Caching Files');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// アクティベート時の処理
-self.addEventListener('activate', event => {
-  console.log('Service Worker Activated');
-  
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            console.log('Removing old cache');
-            return caches.delete(cacheName);
-          }
-        })
-      );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
-});
-
-// リクエストのフェッチ時の処理
-self.addEventListener('fetch', event => {
-  console.log('Fetching', event.request.url);
-  
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
-});
+}); 
